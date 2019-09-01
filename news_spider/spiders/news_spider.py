29eea38732eaa8d30c872433e83ae72c1cd7694c
Felipe-ZC <fzunigacar@gmail.com>
2019-08-31 22:38:40 -0700

    first commit, very rough version of the captured tracks spider

---
+++ news_spider/spiders/news_spider.py
@@ -0,0 +1,26 @@
+import scrapy
+
+class NewsSpider(scrapy.Spider):
+    name = "news"
+    start_urls = ['https://capturedtracks.com/news/']
+    
+    # Keep note of the strategies used for each website,
+    # they'll change as we go along!
+    
+    def parse(self, response):
+        print('Parsing webpage...')
+        data = response.xpath('//article')
+        print('Found ' + str(len(data)) + ' articles')
+        # print(data[0].getall())
+        counter = 1
+        for item in data:
+            print('------------- Parsing article #{} -------------'.format(counter))
+            print(item.getall())
+            counter = counter + 1
+            # print(item.xpath('.//header').getall())
+            yield {
+                "title" : item.xpath('.//header/a/h1/text()').get(),
+                "date" : item.xpath('.//header/a/time/text()').get(),
+                "preview" : item.xpath('.//div[@class="news-item--preview"]/p/text()').get(),
+            }
+

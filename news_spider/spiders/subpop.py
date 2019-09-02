a580a3d2bbb940e7a1614011694006bba9b38f94
Felipe-ZC <fzunigacar@gmail.com>
2019-09-02 11:22:15 -0700

    Changed preview xpath, now its select ALL text nodes in the article, working on only selecting nodes after the header element

--- news_spider/spiders/subpop.py
+++ news_spider/spiders/subpop.py
@@ -24,8 +24,15 @@ class NewsSpider(scrapy.Spider):
                 "title" :item.xpath('.//header/h2/a/text()').get(),
                 "date" : item.xpath('.//header/p/text()').getall(), 
                 # NOTE: There are <span> tags after some of the <p> elements for preview
-                # elements which our path is not accounting for. 
-                "preview" : item.xpath('./p/text() | ./ul/li/p/text()').getall(),
+                # elements which our path is not accounting for.
+
+                # TODO: Change this xpath to select any tag with a path ending in text()
+                # except for <div> and <header>.
+                # "preview" : item.xpath('./p/text() | ./ul/li/p/text()').getall()
+                # descendant refers to all children, grandchildren etc. of the current node,
+                # in this case the current node is article. The xpath below selects all text
+                # node desecendants of an article tag. 
+                "preview" : item.xpath('./descendant::text()').getall(),
             }
         print('------------- Done parsing -------------')
         # next_page = response.xpath('//div[@class="wrapper"]/a/@href').get()

23bf22c4e578675e2b56be8c856a48482d2e2127
Felipe-ZC <fzunigacar@gmail.com>
2019-09-17 23:06:11 -0700

    code cleanup

--- mu_spider/spiders/artists/subpop_artists.py
+++ mu_spider/spiders/artists/subpop_artists.py
@@ -1,5 +1,4 @@
 #TODO: Accept args for page limit and offset. 
-
 import scrapy
 import re
 
@@ -12,17 +11,9 @@ class SubpopSpider(scrapy.Spider):
         self.follow = follow
     
     def parse(self, response):
-        print('Parsing webpage...')
         data = response.xpath('//section[@class="alpha-list"]')
         for item in data:
-            print('------------- Parsing article -------------')
-            yield {
-                "name": item.xpath('./ul/li/a/text()').getall(),
-            }
+            print('------------- Parsing element -------------')
+            yield { "artists": item.xpath('./ul/li/a/text()').getall() }
         print('------------- Done parsing -------------')
-        #TODO: Find a way to use case for link following on artists page or remove this... 
-        # if self.follow:
-            # next_page = response.xpath('//div[@class="pagination"]/span[@class="next"]/a/@href').get()
-            # if next_page:
-                # yield response.follow(next_page, callback=self.parse)
 

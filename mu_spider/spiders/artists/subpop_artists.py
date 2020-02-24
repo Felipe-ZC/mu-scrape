4e717a3e6da489f63df3f9d3fbdef7489b0c6a51
Felipe-ZC <fzunigacar@gmail.com>
2020-02-23 22:53:44 -0800

    Added all current artists (Subpop) to one dict

--- mu_spider/spiders/artists/subpop_artists.py
+++ mu_spider/spiders/artists/subpop_artists.py
@@ -4,16 +4,16 @@ import re
 
 class SubpopSpider(scrapy.Spider):  
     name = 'subpop_artists' 
-    start_urls = ['https://www.subpop.com/artists/list']
+    start_urls = ['https://www.subpop.com/artists']
     
     def __init__(self, follow=None, *args, **kwargs): 
         super(SubpopSpider, self).__init__(*args, **kwargs)
         self.follow = follow
     
     def parse(self, response):
-        data = response.xpath('//section[@class="alpha-list"]')
+        data = response.xpath('//ul[@id="current-artists"]')
         for item in data:
             print('------------- Parsing element -------------')
-            yield { "artists": item.xpath('./ul/li/a/text()').getall() }
+            yield { "artists": item.xpath('./li/article/div/h3[@class="artist-name"]/a/text()').getall() }
         print('------------- Done parsing -------------')
 

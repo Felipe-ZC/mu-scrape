3161e33d893b69785a7912185f6cc81920b81e79
Felipe-ZC <fzunigacar@gmail.com>
2020-02-26 21:55:56 -0800

    Added media to pf news spider and text_content

--- mu_spider/settings.py
+++ mu_spider/settings.py
@@ -68,9 +68,9 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-   # 'mu_spider.pipelines.DataPipeline': 300,
-# }
+ITEM_PIPELINES = {
+   'mu_spider.pipelines.DataPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
--- mu_spider/spiders/news/pitchfork.py
+++ mu_spider/spiders/news/pitchfork.py
@@ -11,27 +11,31 @@ class PitchforkSpider(CrawlSpider):
     name = 'pitchfork_news'
     start_urls = ['https://www.pitchfork.com/news/?page=1']
     
+    rules = (
+            Rule(LinkExtractor(restrict_xpaths=('//div[contains(@class, "news-module")]','//link[@rel="next"]'), tags=['a', 'link']), callback="parse_items", follow=False),
+    )
+
     def __init__(self, follow=None, *args, **kwargs): 
         super(PitchforkSpider, self).__init__(*args, **kwargs)
         self.follow = follow
-        self.rules = (
-            Rule(LinkExtractor(restrict_xpaths=('//div[contains(@class, "news-module")]','//link[@rel="next"]'), tags=['a', 'link']), callback="parse_items", follow=self.follow),
-        )
 
     
     def parse_items(self, response):
         print('Parsing webpage...')
         print(response)
+        media = response.xpath('//img/@src | //iframe/@src')
         data = response.xpath('//div[@class="article-content"]')
         ws = ' '
+        print('html content is')
         if(re.match(r'\?page=\d$',response.url) == None):
             for item in data:
                 print('------------- Parsing article -------------')
                 print(item.getall())
-                #TODO: xpath: .// vs //
                 yield {
                     "title": item.xpath('.//header/h1/text()').get(),
-                    "subtitle": item.xpath('.//header/h2/p/text()').get(),
-                    "content": item.xpath('.//div[@class="contents"]').get(),
+                    "subtitle": item.xpath('.//header/p/text()').get(),
+                    # "content": item.xpath('.//div[@class="contents"]/p/text()').getall(),
+                    "text_content": ws.join(item.xpath('.//div[@class="contents"]//text()').getall()),
+                    "media": media.getall(),
                 }
             print('------------- Done parsing -------------')
